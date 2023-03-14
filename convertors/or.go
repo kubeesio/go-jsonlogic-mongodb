fee4e567d639945cd673f4a9d0235fe5ba7af3ab
Stallos11 <vincentdelev@yahoo.fr>
2023-03-14 15:16:35 +0100

    feat: or operator

---
+++ convertors/or.go
@@ -0,0 +1,28 @@
+package convertors
+
+import (
+	"errors"
+
+	"github.com/kubeesio/go-jsonlogic-mongodb/helpers"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func ConvertOr(value interface{}) (bson.D, error) {
+	if !helpers.IsSlice(value) {
+		return nil, errors.New("value must be a slice with two arguments")
+	}
+
+	var internalError error
+
+	firstArgument, internalError := InternalConvert(value.([]interface{})[0])
+	if internalError != nil {
+		return nil, internalError
+	}
+
+	secondArgument, internalError := InternalConvert(value.([]interface{})[1])
+	if internalError != nil {
+		return nil, internalError
+	}
+
+	return bson.D{{Key: "$or", Value: bson.A{firstArgument, secondArgument}}}, nil
+}
